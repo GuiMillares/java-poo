38c3d9b24e7ce644d42fe556aa07666ca50d5507
Bowie <108934989+GuiMillares@users.noreply.github.com>
2023-07-26 00:16:18 -0300

    Adicionando uma nova classe e refatorando o código anterior

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -1,8 +1,5 @@
 package poo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args){
       
---
+++ src/poo/Produto.java
@@ -0,0 +1,6 @@
+package poo;
+
+public class Produto{
+
+    
+}
